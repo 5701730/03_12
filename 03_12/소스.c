ec74c56b857db91256305819ae79cab67cc6cbb7
8933hkt3618@gmail.com <8933hkt3618@gmail.com>
2024-03-12 10:30:22 +0900

    last commit.

--- 03_12/소스.c
+++ 03_12/소스.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 // 0부터 100까지의 숫자 중에서 소숟ㄹ 만의 합을 구하는 프로그램을 작성한다.
 int main() {
-	int sum = 0;
 	int num = 0;
+	int sum = 0;
 	for (int num = 2; num <= 100; num++) {
 		int count = 0;
 		for (int i = 1; i <= num; i++)
