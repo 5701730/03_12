b5855f0fe74623b7d5cd6047c2c31b63efce2a92
8933hkt3618@gmail.com <8933hkt3618@gmail.com>
2024-03-12 10:23:23 +0900

    second commit

--- 03_12/소스.c
+++ 03_12/소스.c
@@ -2,15 +2,19 @@
 // 0부터 100까지의 숫자 중에서 소숟ㄹ 만의 합을 구하는 프로그램을 작성한다.
 int main() {
 	int sum = 0;
-
-	for (int i = 2; i <= 100; i++) {
+	int num = 0;
+	for (int num = 2; num <= 100; num++) {
 		int count = 0;
-		for (int j = 1; j <= i; j++)
-			if (i % j == 0)
+		for (int i = 1; i <= num; i++)
+			if (num % i == 0)
 				count++;
-
+		if (count == 2)
+			sum += num;
+		
 	}
+	
 
+	printf("합:%d", sum);
 
 
 	return 0;
